499b4146bfee4114bb49b1af8ea36c0b271fe15f
Kazuya Arita <135966181+kazuya-arita@users.noreply.github.com>
2023-07-19 06:51:16 +0000

    課題途中２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,13 @@
 class BooksController < ApplicationController
  
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+     redirect_to book_path(@book.id)
+    else
+     render :index
+    end
   end
   
   def index
@@ -18,7 +22,13 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end  
+  
   def destroy
     book = Book.find(params[:id])
     book.destroy
